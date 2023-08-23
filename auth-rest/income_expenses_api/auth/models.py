1b1869b5cc2e693e8f11743a48e39fc736daca25
Mina Nilay TEZER <107746643+Mimakk@users.noreply.github.com>
2023-08-24 00:19:22 +0300

    added create_user to UserManager class

--- auth-rest/income_expenses_api/auth/models.py
+++ auth-rest/income_expenses_api/auth/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+from django.contrib.auth.models import (AbstractBaseUser, BaseUserManager, PermissionMixin)
+
+from django.db import models 
+
+
+class UserManager(BaseUserManager):
+    
+    def create_user(self, username, email, password=None):
+        
+        if username is None:
+            raise TypeError('Users should have a username')
+        if email is None:
+            raise TypeError('Users should have a email')
+        
+        user=self.model(username=username, email=self.normalize_email(email))
+        user.set_password(password)
+        user.save()
